28cf175efe863790c2dce63c9ae876db3b4563ad
mishrabhi <asmyselfabhishek00@gmail.com>
2024-06-24 01:13:39 +0530

    added display data function

--- Fetch/country-information/index.js
+++ Fetch/country-information/index.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   fetchCountries();
 });
 
-let countryContainer = document.getElementById("country-container");
-
+//function to fetch country data
 async function fetchCountries() {
   {
     try {
@@ -11,9 +10,33 @@ async function fetchCountries() {
         `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries`
       );
       let countryData = await res.json();
-      console.log(countryData);
+      countries = countryData.data; //getting the data required to display
+      displayCountries(countries); //calling displayCountries function to display data
     } catch (error) {
       console.log("Something went wrong!");
     }
   }
+}
+
+//dunction to display country data
+function displayCountries(countries) {
+  countryContainer = document.getElementById("country-container");
+  countryContainer.innerHTML = "";
+  countries.forEach((country) => {
+    const countryCard = document.createElement("div");
+    countryCard.className = "country-card";
+
+    const name = document.createElement("h2");
+    name.innerText = country.country;
+    console.log(name);
+
+    const rank = document.createElement("h6");
+    rank.innerText = country.Rank;
+
+    const population = document.createElement("p");
+    population.innerText = country.population;
+
+    countryCard.append(name, rank, population);
+    countryContainer.append(countryCard);
+  });
 }
