745599d810531fe5498971451021718a99d30c9e
mishrabhi <asmyselfabhishek00@gmail.com>
2024-06-29 13:55:59 +0530

    added filter and pagination

--- Fetch/sorting-filtering-pagination/index.js
+++ Fetch/sorting-filtering-pagination/index.js
@@ -1,8 +1,21 @@
-const url = `http://localhost:3000/products`;
+const baseUrl = `http://localhost:3000/products`;
 
-const getData = async (url) => {
+const getTotalCount = async () => {
   try {
-    let res = await fetch(`${url}`);
+    let res = await fetch(baseUrl);
+    let products = await res.json();
+    return products.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getData = async (url, queryparams = "") => {
+  try {
+    let res = await fetch(`${url}${queryparams}`);
+    let totalCount = await getTotalCount(); //total number of data
+    pagination(totalCount, 6);
+
     let products = await res.json();
     console.log(products);
     displayData(products);
@@ -11,7 +24,7 @@ const getData = async (url) => {
   }
 };
 
-getData(url);
+getData(`${baseUrl}?_page=1&_limit=6`);
 
 function displayData(products) {
   let container = document.getElementById("container");
@@ -33,18 +46,17 @@ function displayData(products) {
     category.textContent = ele.category;
 
     let price = document.createElement("p");
-    price.textContent = ele.price;
+    price.textContent = `$${ele.price}`;
 
     let rating = document.createElement("p");
-    rating.textContent = ele.rating.rate;
+    rating.textContent = `Rating: ${ele.rating.rate}`;
 
     card.append(image, title, description, category, price, rating);
     container.append(card);
   });
 }
 
-//sorting
-
+// Sorting
 let sort = document.getElementById("sort");
 sort.addEventListener("input", function () {
   sortDataByPrice();
@@ -53,5 +65,32 @@ sort.addEventListener("input", function () {
 function sortDataByPrice() {
   let sortVal = sort.value;
   console.log(sortVal);
-  getData(`${url}?_sort=price&_order=${sortVal}`);
+  getData(`${baseUrl}?_page=1&_limit=6`, `&_sort=price&_order=${sortVal}`);
+}
+
+// Filtering
+let filter = document.getElementById("filter");
+filter.addEventListener("input", function () {
+  filterDataByCategory();
+});
+
+function filterDataByCategory() {
+  let filterValue = filter.value;
+  getData(`${baseUrl}?_page=1&_limit=6`, `&category=${filterValue}`);
+}
+
+// Pagination
+let paginationDiv = document.getElementById("pagination-wrapper");
+function pagination(total, limit) {
+  paginationDiv.innerHTML = "";
+  let numberOfBtn = Math.ceil(total / limit); // no. of pagination buttons required to display total data
+  console.log(numberOfBtn);
+  for (let i = 1; i <= numberOfBtn; i++) {
+    let btn = document.createElement("button");
+    btn.textContent = i;
+    btn.addEventListener("click", function () {
+      getData(`${baseUrl}?`, `_page=${i}&_limit=6`);
+    });
+    paginationDiv.append(btn);
+  }
 }
