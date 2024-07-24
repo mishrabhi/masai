c658dbc6508f501f12d08b28b60773c72e7f22a8
mishrabhi <asmyselfabhishek00@gmail.com>
2024-07-24 16:22:16 +0530

    problem 2

--- Factory & Constructor Function/Class Inheritance/assignment.js
+++ Factory & Constructor Function/Class Inheritance/assignment.js
@@ -17,15 +17,46 @@ class Person {
   }
 }
 let p1 = new Person("Sumit", 26);
-console.log(p1);
-p1.canSleep();
-Person.greet();
+// console.log(p1);
+// p1.canSleep();
+// Person.greet();
 
 //   #### Question 2: Create a class named Employee that inherits from Person.
 //   - Add a private property called salary and create a getter and setter for it.
 //     - The getter should return the salary.
 //     - The setter should validate that the salary is a positive number. If it is, update the salary; otherwise, print "Salary must be a positive number." in the console.
 //   - Add a method to the prototype called working that will return "<name> is working". Here <name> should be the name of the person.
+
+class Employee extends Person {
+  #salary;
+  constructor(name, age, salary) {
+    super(name, age);
+    this.#salary = salary;
+  }
+
+  get salary() {
+    return this.#salary;
+  }
+
+  set salary(val) {
+    this.#salary = val;
+    if (this.#salary >= 0) {
+      return this.#salary;
+    } else {
+      return "Salary must be a positive number";
+    }
+  }
+
+  working() {
+    console.log(`${this.name} is working`);
+  }
+}
+
+let E1 = new Employee("Sumit", 24, 200000);
+console.log(E1.salary);
+E1.salary = 250000;
+console.log(E1.salary);
+E1.working();
 
 //   #### Question 3: Create a class named Manager that inherits from Employee.
 //   - Add a static method called getRole that returns the string "Manager".
